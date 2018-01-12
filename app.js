d971238204b127d221210c01031c82d6d7f3cbaf
shubham <shubhamj7997@gmail.com>
2018-01-12 13:37:02 +0530

    created a static file handler in app

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const Webapp = require('./webapp.js');
+const fs = require('fs');
+
+const getContentType = function(path){
+  let fileExt = path.split(".").slice(-1)[0];
+  let contentTypes = {
+    'js':"text/javascript",
+    'html':'text/html',
+    'img':'img/gif',
+  }
+  return contentTypes[fileExt];
+}
 
 let app = Webapp.create();
 
-app.postUse((req,res)=>{
+const serveFileNotFound = (req,res)=>{
   if(!res.finished){
     res.statusCode = 404;
     res.write("file not Found");
     res.end();
   }
-});
+}
+
+const serveStaticFiles = (req,res)=>{
+  if(!res.finished && fs.existsSync("./public"+req.url)){
+    res.statusCode =200;
+    res.setHeader('content-type',getContentType(req.url));
+    res.write(fs.readFileSync("./public"+req.url))
+    res.end();
+  };
+}
+
+app.postUse(serveStaticFiles);
+app.postUse(serveFileNotFound);
 
 module.exports = app;
